cbe1761c4ab05779d0aeb25edc97c64fd265418d
lengway <lengway@inbox.ru>
2025-05-04 12:10:16 +0500

    реализация майхештейбл

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -17,6 +17,7 @@ public class MyHashTable<K, V> {
     }
 
     private int hash(K key) {
+        // я решил реализовать хештейбл только для стрингов поэтому в начале идет проверка
         if (!(key instanceof String strKey)) {
             throw new IllegalArgumentException("Key must be a String");
         }
@@ -33,29 +34,65 @@ public class MyHashTable<K, V> {
 
     public void put(K key, V value) {
         int index = hash(key);
-
         HashNode<K, V> head = ChainArray[index];
 
+        // прост добавляем узел сразу если ну ничо нет
         if (head == null) {
             ChainArray[index] = new HashNode<>(key, value);
             size++;
+            return;
         }
+
+        // если чето да есть проходимся по цепочке
+        HashNode<K, V> current = head;
+        while (current != null) {
+            // и если ключ уже имеется то мы обновляем значение
+            if (current.getKey().equals(key)) {
+                current.setValue(value);
+                return;
+            }
+
+            // если некст нулл значит мы в конце цепочки
+            if (current.getNext() == null) {
+                break;
+            }
+
+            current = current.getNext();
+        }
+
+        // и если мы оказались в конце то добавляем узел в конец
+        current.setNext(new HashNode<>(key, value));
+        size++;
     }
 
 
     public V get(K key) {
-        return null;
+        return ChainArray[hash(key)].getValue();
     }
 
     public V remove(K key) {
-        return null;
+        ChainArray[hash(key)] = null;
+        size--;
+        return ChainArray[hash(key)].getValue();
     }
 
     public boolean contains(V value) {
+        for (HashNode<K, V> kvHashNode : ChainArray) {
+            if (kvHashNode.getValue().equals(value)) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     public K getKey(V value) {
+        for (HashNode<K, V> kvHashNode : ChainArray) {
+            if (kvHashNode.getValue().equals(value)) {
+                return kvHashNode.getKey();
+            }
+        }
+
         return null;
     }
 }
